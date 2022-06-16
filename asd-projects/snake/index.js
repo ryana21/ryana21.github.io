82be2abd4e2c1b096d53b35a31fb8de576b44e3c
ryana21 <mrrwalexander@gmail.com>
2022-06-16 20:07:12 +0000

    savingsnake

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -14,11 +14,15 @@ function runProgram(){
     W: 87,
     A: 65,
     S: 83,
-    D: 68
+    D: 68,
+    enter: 13,
     };
 
   var GRID_SIZE = 40;
   // Game Item Objects
+
+
+
   var head = { 
   id: "#snake", 
   positionX: randomGrid(2400), 
@@ -33,6 +37,10 @@ function runProgram(){
     positionY: randomGrid(1200),
   };
 
+  var snakeArray = [
+    head,
+    
+  ]
 
 
   // one-time setup
@@ -41,8 +49,12 @@ function runProgram(){
   $(document).on('keydown', handleKeyDownA);
   $(document).on('keydown', handleKeyDownS);
   $(document).on('keydown', handleKeyDownD);
+  $(document).on('keydown', handleKeyDownE);
+
+  spawnApple(apple);
+
  
-  $(document).on('keyup', handleKeyUp); 
+  // $(document).on('keyup', handleKeyUp); 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -52,20 +64,26 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    redrawGameItem(head)
-    repositionGameItem(head)
-    spawnApple(apple)
-
-    if (head.positionX === apple.positionX && head.positionY === apple.positionY) {
-
-      
-      
+    for (i = 0; i <= snakeArray.length ; i++){
+      redrawGameItem(snakeArray[i])
+      repositionGameItem(snakeArray[i])
+      if (head.positionX === apple.positionX && head.positionY === apple.positionY) {
+        spawnApple(apple); 
+        addPiece();
+        scoreKeep(); 
+      }
     }
   }
   
   /* 
   Called in response to events.
   */
+  function handleKeyDownE(event) {
+    if (event.which === KEY.enter){
+      addPiece();
+    }
+  }
+  
   function handleKeyDownW(event) {
     if (event.which === KEY.W){
       head.speedY = -40;
@@ -96,20 +114,20 @@ function runProgram(){
 
 //KEYUPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
 
-  function handleKeyUp(event) {
-    if (event.which === KEY.A){
-      head.speedX = 0;
-    }
-    else if (event.which === KEY.W){
-     head.speedY = 0;
-    }
-     else if (event.which === KEY.D){
-     head.speedX = 0;
-    }
-    else if (event.which === KEY.S){
-    head.speedY = 0;
-   }
-  }
+// function handleKeyUp(event) {
+//   if (event.which === KEY.A){
+//     head.speedX = 0;
+//   }
+//   else if (event.which === KEY.W){
+//    head.speedY = 0;
+//   }
+//    else if (event.which === KEY.D){
+//    head.speedX = 0;
+//   }
+//   else if (event.which === KEY.S){
+//   head.speedY = 0;
+//  }
+// }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -132,6 +150,37 @@ function runProgram(){
     $(piece.id).css("top", piece.positionY);
   }
 
+  //new pieceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+  function elementObject(id) {
+    var newPiece = {};
+    
+    newPiece.id = id;
+    newPiece.backgroundColor = parseFloat($(id).css('background-color'));
+    newPiece.borderRadius = parseFloat($(id).css('border-radius'));
+    newPiece.positionX = parseFloat($(id).css('left'))
+    newPiece.positionY = parseFloat($(id).css('top'));
+    newPiece.width = parseFloat($(id).css('width')); 
+    newPiece.height = parseFloat($(id).css('height'));   
+    newPiece.speedX = 0;
+    newPiece.speedY = 0;
+  
+    return newPiece;
+  }
+
+
+  function createPiece(id){
+    $("<div>").attr("id", id)
+              .addClass("snakePart")
+              .appendTo("#board");
+  }
+  
+  function addPiece(){
+    var nextId = "body" + snakeArray.length;
+    createPiece(nextId);
+    var newObject = elementObject("#" + nextId);
+    snakeArray.push(newObject); 
+  }
+
 
   function endGame() {
     // stop the interval timer
@@ -140,8 +189,24 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
+ 
+  //snake pieces
 
-  
+
+
+
+
+  //scoreeeeeeeeeeeeeeeeeee
+  var score = 0;
+  var scoreBox = jQuery('#score');
+  function scoreKeep(){
+    score = score + 1;
+    scoreBox.text("score\n" + score);
+  }
+  scoreBox.text("score\n")
+  jquery.score.text(score);
+
+  //grid spot maker
   function randomGrid(maxPixel){
     return Math.floor(Math.random() * (maxPixel/GRID_SIZE)) * GRID_SIZE;
   }
